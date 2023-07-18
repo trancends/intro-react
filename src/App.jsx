69b3fe0f354c039c84015f5327acc29f8c456b10
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-07-18 20:48:37 +0700

    add tanstack/react-query for caching fetch result. create fetch pet to pass the data cached data into details. refactor usebreedlist make the fetching part into its own function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
-import { BrowserRouter } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
 import Details from "./Details";
-import { Link } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <header>
-          <Link to={"/"}>Adopt Me</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <header>
+            <Link to={"/"}>Adopt Me</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 };
