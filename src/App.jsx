ef370bfe69180ed53a6d6422a1435fa487c3490d
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-07-18 16:42:55 +0700

    add react-router for client side navigation, create details component, pass id from pet component into details page via react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Details from "./Details";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <header>
+          <Link to={"/"}>Adopt Me</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
