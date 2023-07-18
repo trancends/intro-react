b9b6b0b460f0d3237675b943f2564fb17e3c7331
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-07-18 16:00:47 +0700

    make pets section from search params into its own component called results and modify pet.jsx

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Restults";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -89,14 +89,7 @@ const SearchParams = () => {
         <button>Submit</button>
       </form>
 
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
